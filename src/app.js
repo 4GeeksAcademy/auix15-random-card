00e3f106a59b6521b31e38aff74f5ee5f65c2c6d
Aixhua <167004871+auix15@users.noreply.github.com>
2024-09-10 12:46:54 +0000

    firstTry

--- src/app.js
+++ src/app.js
@@ -1,11 +1,55 @@
-/* eslint-disable */
+// eslint-disable
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const suit = ["heart", "club", "spade", "diamond"];
+  const values = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  //función para recorrer el indice del array
+  function generateNewCard() {
+    const randomSuit = suit[Math.floor(Math.random() * suit.length)];
+    const randomValue = values[Math.floor(Math.random() * values.length)];
+
+    const card = document.querySelector(".card");
+
+    //asignar el simbolo a cada palo
+    let symbol;
+    switch (randomSuit) {
+      case "heart":
+        symbol = "♥";
+        break;
+      case "club":
+        symbol = "♣";
+        break;
+      case "spade":
+        symbol = "♠";
+        break;
+      case "diamond":
+        symbol = "♦";
+        break;
+    }
+
+    card.innerHTML = `${randomValue}${symbol}`;
+    card.className = `card ${randomSuit}`;
+  }
+
+  document
+    .getElementById("rollButton")
+    .addEventListener("click", generateNewCard);
+
+  generateNewCard();
 };
